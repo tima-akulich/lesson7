1c92cc1f0d1fd4e75f0e1bd310ae3a714f34d077
rolll13 <r.mikulich@mail.ru>
2018-09-30 21:56:51 +0300

    task2 hw7

---
+++ task2.py
@@ -0,0 +1,17 @@
+import argparse
+import glob
+import os
+
+from PIL import Image
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', required=True, type=str)
+parser.add_argument('--width', required=True, type=int)
+parser.add_argument('--height', required=True, type=int)
+image_thumb = parser.parse_args()
+os.chdir(image_thumb.path)
+
+for i in glob.glob("*.jpg"):
+    image = Image.open(i)
+    image.thumbnail((image_thumb.width, image_thumb.height))
+    image.save('Thumb_'+i)
